9d6afe9c60871e486cc60593cb7e3ea76bb7d82f
karkris41295 <karkris3@hotmail.com>
2016-08-30 00:41:22 +0400

    It works.

---
+++ secant_method.py
@@ -0,0 +1,22 @@
+#Secant Method
+#MATH F313: Numerical Analysis
+
+from math import e
+
+def f(x):
+    return 5*e**-x + x - 5
+    
+x1, x2 = -2., 1.5 # bracket values (x1,x2) = (-2,1.5)
+error = 1e-6 # permissible error
+
+i = 1 # iteration counter
+
+while abs(x1-x2)>error:
+    x3 = x2 - (f(x2)*(x2-x1))/(f(x2)-f(x1)) # secant formula
+    x1, x2 = x2, x3 # updating values for next iteration
+    i+=1
+
+rt = x2 # storing the root as x2
+
+print 'The root of the equation is ' + str(rt) + '\n' + 'f(root) = ' + str(f(rt))
+print 'No. of iterations: ' + str(i)
