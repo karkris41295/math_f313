38ef32a25f6a4978507ef3ef5320a7206b031da0
karkris41295 <karkris3@hotmail.com>
2016-09-08 15:52:15 +0400

    added option to switch to improved newton rapshon for faster convergence

--- newton_raphson.py
+++ newton_raphson.py
@@ -12,6 +12,23 @@ def d_dtf(x):
     Derivative of f(x)
     '''
     return 6*924*x**5 - 5*2772*x**4 + 4*3150*x**3 - 3*1680*x**2 + 420*2*x - 42
+
+def d2f(x):
+    '''
+    Derivative of f'(x) (for improved Newton-Raphson)
+    '''
+    return 5*6*924*x**4 - 4*5*2772*x**3 + 3*4*3150*x**2 - 2*3*1680*x + 420*2
+
+def improve(i, x1):
+    '''
+    Function which optionally uses the improved newton method
+    
+    Takes 1 argument, if 'True', uses the improved method, else uses the regular method
+    '''
+    if i == False:
+        return f(x1)/d_dtf(x1)
+    elif i == True:
+        return (f(x1)*d_dtf(x1))/(d_dtf(x1)**2 - f(x1)*d2f(x1))                
     
 import pylab as plt
 import numpy as np
@@ -25,13 +42,13 @@ def inspect():
     plt.plot(a,b)
     plt.show()
     
-x1, x2 = 0.6, 0. # first guess and default for x2
+x1, x2 = 10., 0. # first guess and default for x2
 error = 1e-6 # permissible error
 numer = 10.
 i = 0 # iteration counter
 
 while numer>error and i < 200:
-    x2 = x1 - f(x1)/d_dtf(x1) # Newton-Raphson formula
+    x2 = x1 - improve(True, x1) # Newton-Raphson formula
     print 'x1 = {0}, x2 = {1}'.format(x1, x2)# test line
     numer = abs(x2-x1) # making sure 6 consecutive digits coincide
     x1 = x2
